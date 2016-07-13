550e93638eb272b5cf46054dfbb77ba8565bc8cf
Mark Hilton <mark.hilton@spafinder.com>
2016-07-13 15:03:46 -0400

    Update for group select drop down template

--- src/views/select.blade.php
+++ src/views/select.blade.php
@@ -9,11 +9,23 @@
     <div @if ($position == 'side') class="col-sm-8" @endif>
 		<select id="field-select-{{ $name }}" class="@if ($class) {{ $class }} @else width100p @endif" name="{{ $name }}" data-placeholder="{{ $value }}">
 			@foreach ($choice as $key => $val)
-			@if (is_null($value))
-				<option @if ($default == $key) selected="selected" @endif value="{{ $key }}">{{ $val }}</option>
-			@else
-				<option @if ($value == $key) selected="selected" @endif value="{{ $key }}">{{ $val }}</option>
-			@endif
+				@if (is_array($val))
+					<optgroup label="{{ $key }}">
+					@foreach ($val as $group_key => $group_val)
+						@if (is_null($value))
+							<option @if ($default == $group_key) selected="selected" @endif value="{{ $group_key }}">{{ $group_val }}</option>
+						@else
+							<option @if ($value == $group_key) selected="selected" @endif value="{{ $group_key }}">{{ $group_val }}</option>
+						@endif
+					@endforeach
+					</optgroup>
+				@else
+					@if (is_null($value))
+						<option @if ($default == $key) selected="selected" @endif value="{{ $key }}">{{ $val }}</option>
+					@else
+						<option @if ($value == $key) selected="selected" @endif value="{{ $key }}">{{ $val }}</option>
+					@endif
+				@endif
 			@endforeach
 		</select>
 	</div>
